a66be17607e9bd4a647e9e46529e2f4da442aeac
Timoteo Junior <timoteojjunior@gmail.com>
2013-08-08 18:04:15 +0200

    fixed migration

--- db/migrate/20130807125315_fix_relationships.rb
+++ db/migrate/20130807125315_fix_relationships.rb
@@ -2,7 +2,7 @@ class FixRelationships < ActiveRecord::Migration
   def up
     drop_table :impacts_periods
     remove_column :impacts, :classification_id
-    remove_column :impacts,  :period_id
+    #remove_column :impacts,  :period_id
     
   end
 
